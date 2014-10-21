f7d48aa21cea550f8aa5c2be94eb9cd24ad8cbbb
Wirwing <irving.cf@gmail.com>
2014-10-21 10:51:42 -0500

    - Se agrega php-activerecord. - Se mueve index.php a raíz de proyecto.

--- cve/index.php
+++ cve/index.php
@@ -1,16 +1,16 @@
 <?php
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 // Prepare app
 $app = new \Slim\Slim(array(
-    'templates.path' => '../templates',
+    'templates.path' => 'templates',
 ));
 
 // Create monolog logger and store logger in container as singleton 
 // (Singleton resources retrieve the same log resource definition each time)
 $app->container->singleton('log', function () {
     $log = new \Monolog\Logger('slim-skeleton');
-    $log->pushHandler(new \Monolog\Handler\StreamHandler('../logs/app.log', \Monolog\Logger::DEBUG));
+    $log->pushHandler(new \Monolog\Handler\StreamHandler('logs/app.log', \Monolog\Logger::DEBUG));
     return $log;
 });
 
@@ -18,7 +18,7 @@ $app->container->singleton('log', function () {
 $app->view(new \Slim\Views\Twig());
 $app->view->parserOptions = array(
     'charset' => 'utf-8',
-    'cache' => realpath('../templates/cache'),
+    'cache' => realpath('templates/cache'),
     'auto_reload' => true,
     'strict_variables' => false,
     'autoescape' => true
@@ -31,7 +31,6 @@ $app->get('/', function () use ($app) {
     $app->log->info("Slim-Skeleton '/' route");
     // Render index view
     $app->render('index.html');
-    //echo "Hello World";
 });
 
 // Run app
