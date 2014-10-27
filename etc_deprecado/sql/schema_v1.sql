856214e47b818f3a7e151444bf87248f9013b344
Wirwing <irving.cf@gmail.com>
2014-10-26 19:28:05 -0600

    Se actualiza el schema con los últimos cambios envíados...

--- etc_deprecado/sql/schema_v1.sql
+++ etc_deprecado/sql/schema_v1.sql
@@ -1,20 +1,5 @@
-#�NOMBRE?
--- version 4.0.9
--- http://www.phpmyadmin.net
---
-#�NOMBRE?
--- Tiempo de generación: 09-10-2014 a las 19:57:31
--- Versión del servidor: 5.6.14
--- Versión de PHP: 5.5.6
-
-SET SQL_MODE = NO_AUTO_VALUE_ON_ZERO;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `cve_fmat_uady`
@@ -38,7 +23,14 @@ CREATE TABLE IF NOT EXISTS `actividadcolaborativa` (
   `Adjuntos` bit(1) NOT NULL,
   `Indicadores_Personalizados` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`IDActividad`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `actividadcolaborativa`
+--
+
+INSERT INTO `actividadcolaborativa` (`IDActividad`, `IdTutor`, `Fecha`, `Tipo`, `EstrategiaInstruccional`, `DuracionMin`, `Nombre`, `Descripcion`, `Adjuntos`, `Indicadores_Personalizados`) VALUES
+(1, 10, '2014-10-09', 'CONSTRUCTIVISTA', 'ROMPECABEZAS', 50, 'Resolver el problema adjunto con estructura WHILE', '', b'0', 1);
 
 -- --------------------------------------------------------
 
@@ -76,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `chat_extendido` (
 
 CREATE TABLE IF NOT EXISTS `chat_log` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `IdGpo` int(11) NOT NULL,
+  `IdChatSesion` int(11) NOT NULL,
   `IdUsuario` int(11) NOT NULL,
   `IdActividad` int(11) NOT NULL,
   `IdIndicador` int(11) NOT NULL,
@@ -103,22 +95,6 @@ CREATE TABLE IF NOT EXISTS `gruposcolaborativos` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `indicadores`
---
-
-CREATE TABLE IF NOT EXISTS `indicadores` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(30) NOT NULL,
-  `Descripcion` varchar(30) NOT NULL,
-  `Valor_Sugerido` varchar(30) NOT NULL,
-  `Fecha` date NOT NULL,
-  `IDAutor` varchar(30) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `link_actividadcolaborativa_archivos`
 --
 
@@ -132,18 +108,23 @@ CREATE TABLE IF NOT EXISTS `link_actividadcolaborativa_archivos` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `link_actividadcolaborativa_indicadores`
+-- Estructura de tabla para la tabla `link_actividad_modelo`
 --
 
-CREATE TABLE IF NOT EXISTS `link_actividadcolaborativa_indicadores` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `link_actividad_modelo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `IdActividad` int(11) NOT NULL,
-  `IdIndicador` int(11) NOT NULL,
-  `Valor_Nvo` int(11) NOT NULL,
-  `Valor_tolerancia` int(11) NOT NULL,
-  `IDAutor` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `IdModelo` int(11) NOT NULL,
+  `Estado` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='Tabla intermedia para relacional actividad colaborativa con el modelo de estados' AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `link_actividad_modelo`
+--
+
+INSERT INTO `link_actividad_modelo` (`id`, `IdActividad`, `IdModelo`, `Estado`) VALUES
+(1, 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -162,6 +143,35 @@ CREATE TABLE IF NOT EXISTS `link_gruposcolaborativos_usuarios` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `modelo_estados_config`
+--
+
+CREATE TABLE IF NOT EXISTS `modelo_estados_config` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `IdModelo` int(11) NOT NULL,
+  `Nombre_indicador` char(20) NOT NULL,
+  `Frec_Indiv` tinyint(4) NOT NULL,
+  `Frec_Gpo` tinyint(4) NOT NULL,
+  `Tolerancia` tinyint(4) NOT NULL,
+  `Frec_Indiv2` tinyint(4) DEFAULT NULL,
+  `Frec_Gpo2` tinyint(4) DEFAULT NULL,
+  `Tolerancia2` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='Tabla para configurar el modelo de estados CLL' AUTO_INCREMENT=8 ;
+
+--
+-- Volcado de datos para la tabla `modelo_estados_config`
+--
+
+INSERT INTO `modelo_estados_config` (`ID`, `IdModelo`, `Nombre_indicador`, `Frec_Indiv`, `Frec_Gpo`, `Tolerancia`, `Frec_Indiv2`, `Frec_Gpo2`, `Tolerancia2`) VALUES
+(4, 1, 'argumento', 30, 30, 10, NULL, NULL, NULL),
+(5, 1, 'idea', 20, 30, 10, NULL, NULL, NULL),
+(6, 1, 'duda', 20, 20, 15, NULL, NULL, NULL),
+(7, 1, 'pregunta', 20, 20, 10, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `moodle_rcd`
 --
 
@@ -173,13 +183,12 @@ CREATE TABLE IF NOT EXISTS `moodle_rcd` (
   `IdCourse` int(11) NOT NULL,
   `CourseName` varchar(100) NOT NULL,
   `EmailSesion` varchar(100) NOT NULL,
-  `FechaIni` datetime NOT NULL,
+  `FechaIni` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `FechaFin` datetime DEFAULT NULL,
   `CloseSesion` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=54 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
 
--- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `usuarios`
@@ -190,7 +199,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `NOMBRE` varchar(30) NOT NULL,
   `AP_PAT` varchar(30) NOT NULL,
   `AP_MAT` varchar(30) NOT NULL,
-  `USUARIO` varchar(8) NOT NULL,
+ `USUARIO` varchar(8) NOT NULL,
   `PASSWORD` varchar(8) NOT NULL,
   `SEXO` bit(1) DEFAULT NULL,
   `CIUDAD` varchar(30) DEFAULT NULL,
@@ -198,7 +207,3 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `EMAIL` varchar(80) DEFAULT NULL,
   PRIMARY KEY (`IDUsuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Usuarios registrados en el Juego CLL' AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
