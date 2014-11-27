543c0d00f6251f116e9d87bcec9ce879627cbdaa
Mauricio Lara Soberanis <mauricio9308@gmail.com>
2014-11-27 11:39:49 -0600

    Modified db definitio

--- flyway/sql/V1__initial.sql
+++ flyway/sql/V1__initial.sql
@@ -2,207 +2,240 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
--- -----------------------------------------------------
--- Table `db_cve_fmat_uady`.`modelos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_cve_fmat_uady`.`modelos` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `is_default` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1
-COMMENT = 'Tabla intermedia para relacional actividad colaborativa con el modelo de estados';
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:8889
+-- Generation Time: Nov 27, 2014 at 06:30 PM
+-- Server version: 5.5.34
+-- PHP Version: 5.5.10
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- -----------------------------------------------------
--- Table `db_cve_fmat_uady`.`actividades_colaborativas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_cve_fmat_uady`.`actividades_colaborativas` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `id_modelo` INT(11) NOT NULL,
-  `id_tutor` INT(11) NOT NULL,
-  `nombre` VARCHAR(50) NOT NULL,
-  `descripcion` VARCHAR(200) NOT NULL,
-  `fecha` DATE NOT NULL,
-  `tipo` VARCHAR(50) NOT NULL,
-  `estrategia_instruccional` VARCHAR(50) NOT NULL,
-  `duracion_minima` INT(11) NOT NULL,
+--
+-- Database: `db_cve_fmat_uady`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `actividades_colaborativas`
+--
+
+CREATE TABLE `actividades_colaborativas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_modelo` int(11) NOT NULL,
+  `id_tutor` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `fecha` date NOT NULL,
+  `tipo` varchar(50) NOT NULL,
+  `estrategia_instruccional` varchar(50) NOT NULL,
+  `duracion_minima` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_actividad_colaborativa_modelo_idx` (`id_modelo` ASC),
-  CONSTRAINT `fk_actividad_colaborativa_modelo`
-    FOREIGN KEY (`id_modelo`)
-    REFERENCES `db_cve_fmat_uady`.`modelos` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  KEY `fk_actividad_colaborativa_modelo_idx` (`id_modelo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 
--- -----------------------------------------------------
--- Table `db_cve_fmat_uady`.`actividad_colaborativa_archivos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_cve_fmat_uady`.`actividad_colaborativa_archivos` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `id_actividad` INT(11) NOT NULL,
-  `id_archivo_adjunto` INT(11) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `actividad_colaborativa_archivos`
+--
+
+CREATE TABLE `actividad_colaborativa_archivos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_actividad` int(11) NOT NULL,
+  `id_archivo_adjunto` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `archivos_adjuntos`
+--
+
+CREATE TABLE `archivos_adjuntos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_actividad` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`,`id_actividad`),
+  KEY `fk_archivos_adjuntos_actividad_colaborativa1_idx` (`id_actividad`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_extendido`
+--
+
+CREATE TABLE `chat_extendido` (
+  `id` int(11) NOT NULL,
+  `acuerdo` int(11) NOT NULL,
+  `no_acuerdo` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_logs`
+--
+
+CREATE TABLE `chat_logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_grupo` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_actividad` int(11) NOT NULL,
+  `id_indicador` int(11) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `message` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
 
 
--- -----------------------------------------------------
--- Table `db_cve_fmat_uady`.`archivos_adjuntos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_cve_fmat_uady`.`archivos_adjuntos` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `id_actividad` INT(11) NOT NULL,
-  `nombre` VARCHAR(100) NOT NULL,
-  `url` VARCHAR(100) NOT NULL,
-  `fecha` DATE NOT NULL,
-  PRIMARY KEY (`id`, `id_actividad`),
-  INDEX `fk_archivos_adjuntos_actividad_colaborativa1_idx` (`id_actividad` ASC),
-  CONSTRAINT `fk_archivos_adjuntos_actividad_colaborativa1`
-    FOREIGN KEY (`id_actividad`)
-    REFERENCES `db_cve_fmat_uady`.`actividades_colaborativas` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grupos_colaborativos`
+--
+
+CREATE TABLE `grupos_colaborativos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tutor` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `num_integrantes` int(11) NOT NULL DEFAULT '3',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 
--- -----------------------------------------------------
--- Table `db_cve_fmat_uady`.`chat_extendido`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_cve_fmat_uady`.`chat_extendido` (
-  `id` INT(11) NOT NULL,
-  `acuerdo` INT(11) NOT NULL,
-  `no_acuerdo` INT(11) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `db_cve_fmat_uady`.`chat_logs`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_cve_fmat_uady`.`chat_logs` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `id_chat_sesion` INT(11) NOT NULL,
-  `id_usuario` INT(11) NOT NULL,
-  `id_actividad` INT(11) NOT NULL,
-  `id_indicador` INT(11) NOT NULL,
-  `fecha` DATETIME NOT NULL,
-  `log` VARCHAR(500) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+--
+-- Table structure for table `grupos_colaborativos_usuarios`
+--
 
-
--- -----------------------------------------------------
--- Table `db_cve_fmat_uady`.`grupos_colaborativos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_cve_fmat_uady`.`grupos_colaborativos` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `id_tutor` INT(11) NOT NULL,
-  `fecha` DATE NOT NULL,
-  `nombre` VARCHAR(50) NOT NULL,
-  `num_integrantes` INT(11) NOT NULL DEFAULT 3,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `db_cve_fmat_uady`.`usuarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_cve_fmat_uady`.`usuarios` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(30) NOT NULL,
-  `apellido_paterno` VARCHAR(30) NOT NULL,
-  `apellido_materno` VARCHAR(30) NOT NULL,
-  `usuario` VARCHAR(8) NOT NULL,
-  `password` VARCHAR(8) NOT NULL,
-  `sexo` TINYINT(1) NULL DEFAULT NULL,
-  `ciudad` VARCHAR(30) NULL DEFAULT NULL,
-  `estado` VARCHAR(30) NULL DEFAULT NULL,
-  `email` VARCHAR(80) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COMMENT = 'Usuarios registrados en el Juego CLL';
-
-
--- -----------------------------------------------------
--- Table `db_cve_fmat_uady`.`grupos_colaborativos_usuarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_cve_fmat_uady`.`grupos_colaborativos_usuarios` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `group_id` INT(11) NOT NULL,
-  `user_id` INT(11) NOT NULL,
-  `activity_id` INT(11) DEFAULT NULL,
+CREATE TABLE `grupos_colaborativos_usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `activity_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_grupos_colaborativos_usuarios_grupos_colaborativos1_idx` (`group_id` ASC),
-  INDEX `fk_grupos_colaborativos_usuarios_usuarios1_idx` (`user_id` ASC),
-  INDEX `fk_grupos_colaborativos_usuarios_actividad_colaborativa1_idx` (`activity_id` ASC),
-  CONSTRAINT `fk_grupos_colaborativos_usuarios_grupos_colaborativos1`
-    FOREIGN KEY (`group_id`)
-    REFERENCES `db_cve_fmat_uady`.`grupos_colaborativos` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_grupos_colaborativos_usuarios_usuarios1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `db_cve_fmat_uady`.`usuarios` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_grupos_colaborativos_usuarios_actividad_colaborativa1`
-    FOREIGN KEY (`activity_id`)
-    REFERENCES `db_cve_fmat_uady`.`actividades_colaborativas` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  KEY `fk_grupos_colaborativos_usuarios_grupos_colaborativos1_idx` (`group_id`),
+  KEY `fk_grupos_colaborativos_usuarios_usuarios1_idx` (`user_id`),
+  KEY `fk_grupos_colaborativos_usuarios_actividad_colaborativa1_idx` (`activity_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 
--- -----------------------------------------------------
--- Table `db_cve_fmat_uady`.`indicadores`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_cve_fmat_uady`.`indicadores` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `id_modelo` INT(11) NOT NULL,
-  `nombre` CHAR(20) NOT NULL,
-  `frecuencia_individual` TINYINT(4) NOT NULL,
-  `frecuencia_grupo` TINYINT(4) NOT NULL,
-  `tolerancia` TINYINT(4) NOT NULL,
-  PRIMARY KEY (`id`, `id_modelo`),
-  INDEX `fk_indicadores_modelo1_idx` (`id_modelo` ASC),
-  CONSTRAINT `fk_indicadores_modelo1`
-    FOREIGN KEY (`id_modelo`)
-    REFERENCES `db_cve_fmat_uady`.`modelos` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1
-COMMENT = 'Tabla para configurar el modelo de estados CLL';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `indicadores`
+--
+
+CREATE TABLE `indicadores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_modelo` int(11) NOT NULL,
+  `nombre` char(20) NOT NULL,
+  `frecuencia_individual` tinyint(4) NOT NULL,
+  `frecuencia_grupo` tinyint(4) NOT NULL,
+  `tolerancia` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`,`id_modelo`),
+  KEY `fk_indicadores_modelo1_idx` (`id_modelo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='Tabla para configurar el modelo de estados CLL' AUTO_INCREMENT=5 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modelos`
+--
+
+CREATE TABLE `modelos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `is_default` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='Tabla intermedia para relacional actividad colaborativa con el modelo de estados' AUTO_INCREMENT=2 ;
 
 
--- -----------------------------------------------------
--- Table `db_cve_fmat_uady`.`moodle_rcd`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_cve_fmat_uady`.`moodle_rcd` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `id_user` INT(11) NOT NULL,
-  `username` VARCHAR(100) NOT NULL,
-  `rol` TINYINT(4) NOT NULL,
-  `id_course` INT(11) NOT NULL,
-  `course_name` VARCHAR(100) NOT NULL,
-  `email_sesion` VARCHAR(100) NOT NULL,
-  `fecha_inicio` DATETIME NOT NULL,
-  `fecha_final` DATETIME NULL DEFAULT NULL,
-  `close_sesion` TINYINT(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+--
+-- Table structure for table `moodle_rcd`
+--
+
+CREATE TABLE `moodle_rcd` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `rol` tinyint(4) NOT NULL,
+  `id_course` int(11) NOT NULL,
+  `course_name` varchar(100) NOT NULL,
+  `email_sesion` varchar(100) NOT NULL,
+  `fecha_inicio` datetime NOT NULL,
+  `fecha_final` datetime DEFAULT NULL,
+  `close_sesion` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) NOT NULL,
+  `apellido_paterno` varchar(30) NOT NULL,
+  `apellido_materno` varchar(30) NOT NULL,
+  `usuario` varchar(8) NOT NULL,
+  `password` varchar(8) NOT NULL,
+  `sexo` tinyint(1) DEFAULT NULL,
+  `ciudad` varchar(30) DEFAULT NULL,
+  `estado` varchar(30) DEFAULT NULL,
+  `email` varchar(80) DEFAULT NULL,
+  `tipo` int(11) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Usuarios registrados en el Juego CLL' AUTO_INCREMENT=6 ;
+
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `actividades_colaborativas`
+--
+ALTER TABLE `actividades_colaborativas`
+ADD CONSTRAINT `fk_actividad_colaborativa_modelo` FOREIGN KEY (`id_modelo`) REFERENCES `modelos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `archivos_adjuntos`
+--
+ALTER TABLE `archivos_adjuntos`
+ADD CONSTRAINT `fk_archivos_adjuntos_actividad_colaborativa1` FOREIGN KEY (`id_actividad`) REFERENCES `actividades_colaborativas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `grupos_colaborativos_usuarios`
+--
+ALTER TABLE `grupos_colaborativos_usuarios`
+ADD CONSTRAINT `fk_grupos_colaborativos_usuarios_grupos_colaborativos1` FOREIGN KEY (`group_id`) REFERENCES `grupos_colaborativos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_grupos_colaborativos_usuarios_usuarios1` FOREIGN KEY (`user_id`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `indicadores`
+--
+ALTER TABLE `indicadores`
+ADD CONSTRAINT `fk_indicadores_modelo1` FOREIGN KEY (`id_modelo`) REFERENCES `modelos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- flyway/sql/V2__initial_data_seed.sql
+++ flyway/sql/V2__initial_data_seed.sql
@@ -16,13 +16,15 @@ INSERT INTO `indicadores` (`id`, `id_modelo`, `nombre`, `frecuencia_individual`,
 (4, 1, 'Pregunta', 20, 20, 10);
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Dumping data for table `usuarios`
 --
-INSERT INTO `usuarios` (`id`, `nombre`, `apellido_paterno`,
-	`apellido_materno`, `usuario`, `password`) VALUES
-	(1, 'Irving', 'Caro', 'Fierros', 'wirwing', 'password'),
-	(2, 'Mauricio', 'Lara', 'Soberanis', 'mauricio', 'password'),
-	(3, 'Jaime', 'Negrete', 'Chinchilla', 'chinchil', 'password');
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido_paterno`, `apellido_materno`, `usuario`, `password`, `sexo`, `ciudad`, `estado`, `email`, `tipo`) VALUES
+(1, 'Irving', 'Caro', 'Fierros', 'wirwing', 'password', NULL, NULL, NULL, NULL, 1),
+(2, 'Mauricio', 'Lara', 'Soberanis', 'mauricio', 'password', NULL, NULL, NULL, NULL, 1),
+(3, 'Jaime', 'Negrete', 'Chinchilla', 'chinchil', 'password', NULL, NULL, NULL, NULL, 2),
+(4, 'Ricardo', 'Apellido1', 'Apellido2', 'ricardo', 'password', NULL, NULL, NULL, NULL, 2),
+(5, 'Arit', 'Apellido', 'Apellido', 'arit', 'password', NULL, NULL, NULL, NULL, 2);
 
 --
 -- Volcado de datos para la tabla `actividad_colaborativa`
@@ -30,4 +32,27 @@ INSERT INTO `usuarios` (`id`, `nombre`, `apellido_paterno`,
 INSERT INTO `actividades_colaborativas` (`id`, `id_modelo`, `id_tutor`, `nombre`,
 	`descripcion`, `fecha`, `tipo`, `estrategia_instruccional`,
 	 `duracion_minima`) VALUES
-	(1, 1, 1, 'Resolver el problema adjunto con estructura WHILE', '', '2014-10-09', 'CONSTRUCTIVISTA', 'ROMPECABEZAS', 50);
+	(5, 1, 1, 'Resolver el problema adjunto con estructura WHILE', '', '2014-10-09', 'CONSTRUCTIVISTA', 'ROMPECABEZAS', 50);
+
+	--
+	-- Volcado de datos para la tabla `chat_logs`
+	--
+
+INSERT INTO `chat_logs` (`id`, `id_grupo`, `id_usuario`, `id_actividad`, `id_indicador`, `fecha`, `message`) VALUES
+(10, 1, 4, 5, 3, '2014-11-27 15:19:20', 'Esta es una idea');
+
+
+	--
+	-- Dumping data for table `grupos_colaborativos`
+	--
+
+INSERT INTO `grupos_colaborativos` (`id`, `id_tutor`, `fecha`, `nombre`, `num_integrantes`) VALUES
+(1, 1, '2014-11-26', 'TEST_GROUP', 2);
+
+	--
+	-- Dumping data for table `grupos_colaborativos_usuarios`
+	--
+
+INSERT INTO `grupos_colaborativos_usuarios` (`id`, `group_id`, `user_id`, `activity_id`) VALUES
+(3, 1, 5, 5),
+(4, 1, 4, 5);
