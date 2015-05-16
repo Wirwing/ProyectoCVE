d51666984d9fec897b44326bbb9a6edf57d66ff9
Wirwing <irving.cf@gmail.com>
2015-05-16 09:59:05 -0500

    Se corrige ruta analisis uso.

--- routes/analisis_uso.php
+++ routes/analisis_uso.php
@@ -3,10 +3,10 @@
 /*
 * Rutas del recurso analisis. Renderizan plantillas HTML con los datos obtenidos de la BD.
 */
-$app->get('/teacher/analisis', $auth('teacher'), function () use ($app) {
+$app->get('/teacher/analisis-uso', $auth('teacher'), function () use ($app) {
 
     // Sample log message
-	$app->log->info("Slim-Skeleton '/analisis_uso' route");
+	$app->log->info("Slim-Skeleton '/analisis-uso' route");
     // Render index view
 	$app->render('analisis/index.html');
 
